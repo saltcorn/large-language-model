a79ebb688288c2c4c9c58eb53f656aaed51c54c7
Tom Nielsen <tanielsen@gmail.com>
2024-03-22 15:14:24 +0000

    new models

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ const configuration_workflow = () =>
                     "gpt-3.5-turbo-16k",
                     "gpt-4",
                     "gpt-4-32k",
+                    "gpt-4-turbo-preview",
+                    "gpt-4-1106-preview",
                   ],
                 },
               },
