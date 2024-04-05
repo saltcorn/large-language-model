01db4148aa3f0db2eeb13854efbdfd3fc2eac98c
Tom Nielsen <tanielsen@gmail.com>
2024-04-05 16:15:57 +0100

    new gpt 3.5 models

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ const configuration_workflow = () =>
                   options: [
                     "gpt-3.5-turbo",
                     "gpt-3.5-turbo-16k",
+                    "gpt-3.5-turbo-1106",
+                    "gpt-3.5-turbo-0125",
                     "gpt-4",
                     "gpt-4-32k",
                     "gpt-4-turbo-preview",
