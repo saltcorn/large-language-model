2329f5207af3f9b547406b4b5fd959796888b8b9
Tom Nielsen <tanielsen@gmail.com>
2024-07-25 19:04:57 +0100

    gpt-4o-mini

--- constants.js
+++ constants.js
@@ -5,6 +5,7 @@ const OPENAI_MODELS = [
   "gpt-3.5-turbo-0125",
   "gpt-3.5-turbo-0613",
   "gpt-3.5-turbo-16k-0613",
+  "gpt-4o-mini",
   "gpt-4",
   "gpt-4-32k",
   "gpt-4-turbo-preview",
@@ -18,7 +19,7 @@ const OPENAI_MODELS = [
 const OLLAMA_MODELS_PATH = {
   Darwin: `${process.env.HOME}/.ollama/models`,
   Linux: "/usr/share/ollama/.ollama/models",
-  Windows_NT: "C:\\Users\\%username%\\.ollama\\models."
-}
+  Windows_NT: "C:\\Users\\%username%\\.ollama\\models.",
+};
 
 module.exports = { OPENAI_MODELS, OLLAMA_MODELS_PATH };
