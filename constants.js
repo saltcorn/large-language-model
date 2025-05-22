eaca5b85881828abc238500d416bc375853462d8
Tom Nielsen <tanielsen@gmail.com>
2025-05-22 13:13:37 +0100

    update openai models available

--- constants.js
+++ constants.js
@@ -1,18 +1,19 @@
 const OPENAI_MODELS = [
   "gpt-3.5-turbo",
   "gpt-3.5-turbo-16k",
-  "gpt-3.5-turbo-1106",
-  "gpt-3.5-turbo-0125",
-  "gpt-3.5-turbo-0613",
-  "gpt-3.5-turbo-16k-0613",
   "gpt-4o-mini",
   "gpt-4",
   "gpt-4-32k",
   "gpt-4-turbo-preview",
-  "gpt-4-1106-preview",
-  "gpt-4-0125-preview",
   "gpt-4-turbo",
   "gpt-4o",
+  "gpt-4.1",
+  "gpt-4.1-mini",
+  "gpt-4.1-nano",
+  "o1",
+  "o3",
+  "o3-mini",
+  "o4-mini",
 ];
 
 // https://github.com/ollama/ollama/blob/main/docs/faq.md#where-are-models-stored
