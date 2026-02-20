47a4907040b1ccaf1af2072f86d70579037e5d0e
Christian Hugo <hugo.christian@web.de>
2026-02-20 10:54:26 +0100

    exclude from mobile flag - webpackack fails and I don't think it would make sense to bundle the llm into the app

--- index.js
+++ index.js
@@ -545,6 +545,7 @@ module.exports = {
   functions,
   modelpatterns: require("./model.js"),
   routes,
+  ready_for_mobile: false,
   actions: (config) => ({
     llm_function_call: require("./function-insert-action.js")(config),
     llm_generate: {
